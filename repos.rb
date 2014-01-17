e26915a995f330ef062aaecd5d7947277070ab7f
Andrew Berls <andrew.berls@gmail.com>
2014-01-16 20:58:21 -0800

    Revert to repos.md, and add instructions
    In addition, a minor bug in the script is fixed where the
    CategoryList repo was generating bad markdown.

--- repos.rb
+++ repos.rb
@@ -8,24 +8,28 @@ pass = STDIN.noecho(&:gets).chomp
 puts ''
 
 ORG_NAME = 'UCSB-CS56-Projects'
+OUTPUT   = 'repos.md'
 
 def url_for(category, name)
   "https://www.github.com/#{ORG_NAME}/cs56-#{category}-#{name}/"
 end
 
-client     = Octokit::Client.new(login: username, password: pass)
-repo_names = client.org_repos(ORG_NAME).map(&:name)
+client      = Octokit::Client.new(login: username, password: pass)
+repo_names  = client.org_repos(ORG_NAME).map(&:name)
+repo_groups = Hash.new { |h,k| h[k]                                = [] }
 
-repo_groups = Hash.new { |h,k| h[k] = [] }
 repo_names.each do |name|
+  next if name == "CategoryList" # Hackety hack
+
   _, category, *title = name.split('-')
   title = title.join('-') # Titles can have hyphens
   repo_groups[category] << title
 end
 
-File.open('repos.md', 'w') do |f|
+
+File.open(OUTPUT, 'w') do |f|
   repo_groups.each do |category, repos|
-    f.write "\n\n## #{category}\n"
+    f.write "\n## #{category.capitalize}\n"
 
     repos.each do |name|
       url = url_for(category, name)
